7eac08e4bbf12eb8c079b1800d765e9dd74d5366
Kunal-mob <102619984+Kunal-mob@users.noreply.github.com>
2025-07-01 16:03:25 +0530

    Estate Project React

---
+++ src/components/Contact.jsx
@@ -0,0 +1,94 @@
+import { motion } from "motion/react";
+import React from "react";
+import { toast } from "react-toastify";
+
+const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "34cb495c-7a22-45f3-ad8c-8b5c8e5cdedd");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      toast.success("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      toast.error(data.message);
+      setResult(data.message);
+    }
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: -200 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      viewport={{ once: true }}
+      id="Contact"
+      className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden "
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        Contact
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          With Us
+        </span>
+      </h1>
+      <p className="text-center text-gray-500 mb-8 max-w-80 mx-auto">
+        Ready To Make A Move? Let's Build Your Future Together
+      </p>
+
+      <form
+        onSubmit={onSubmit}
+        className="max-w-2xl mx-auto text-gray-600 pt-8"
+      >
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/2 text-left">
+            Your Name
+            <input
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              type="text"
+              placeholder="Your Name"
+              required
+              name="Name"
+            />
+          </div>
+          <div className="w-full md:w-1/2 text-left md:pl-4">
+            Your Email
+            <input
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              type="email"
+              placeholder="Your Email"
+              required
+              name="Email"
+            />
+          </div>
+        </div>
+        <div className="my-6 text-left">
+          Messege
+          <textarea
+            className="w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none"
+            name="Messege"
+            placeholder="Messege"
+            required
+          ></textarea>
+        </div>
+        <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded">
+          {result ? result : "Send Messege"}
+        </button>
+      </form>
+    </motion.div>
+  );
+};
+
+export default Contact;
